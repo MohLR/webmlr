a1550b1fd303199e5fbdc167f1f9514879ee5312
Mohammad Luthfi Rahmatullah <37324952+MohLR@users.noreply.github.com>
2020-02-20 08:18:49 +0700

    perubahan controller pages

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -8,7 +8,7 @@ class Pages extends CI_Controller {
 			show_404();
 		}
 
-		$data['title'] = ucfirst($page); // Capitalize the first letter
+		// $data['title'] = ucfirst($page); // Capitalize the first letter
 
 		$this->load->view('templates/header', $data);
 		$this->load->view('pages/'.$page, $data);
