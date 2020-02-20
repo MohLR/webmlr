3c6100056904993eee0efa2cd4a041f6cd90d184
Mohammad Luthfi Rahmatullah <37324952+MohLR@users.noreply.github.com>
2020-02-20 16:39:33 +0700

    Perubahan routes dan pages controller, dan penambahan file

---
+++ application/controllers/admin/Overview.php
@@ -0,0 +1,17 @@
+<?php
+class Overview extends CI_Controller {
+    public function __construct()
+    {
+		parent::__construct();
+	}
+
+	public function view($admin = 'overview')
+	{
+		if  (! file_exists(APPPATH.'views/admin/'.$admin.'.php'))
+		{
+			// Whoops, we don't have a page for that!
+			show_404();
+		}
+        $this->load->view('admin/'.$admin);
+	}
+}
