0e3d4b3bdbda4055a142fa393eefaf7d95bbefe7
Mohammad Luthfi Rahmatullah <37324952+MohLR@users.noreply.github.com>
2020-03-14 11:21:02 +0700

    Penhapusan Comment

--- application/views/admin/overview.php
+++ application/views/admin/overview.php
@@ -16,11 +16,6 @@
 		<main>
 			<div class="container-fluid">
 
-			<!-- 
-			karena ini halaman overview (home), kita matikan partial breadcrumb.
-			Jika anda ingin mengampilkan breadcrumb di halaman overview,
-			silahkan hilangkan komentar (//) di tag PHP di bawah.
-			-->
 			<?php $this->load->view("admin/_partials/breadcrumb.php") ?>
 
 			<!-- Icon Cards-->
